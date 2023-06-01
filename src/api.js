ac96564cc4704961f6e1a2e76a6be18150106334
Lee0701 <hgl1002@naver.com>
2023-06-02 07:42:30 +0900

    Convert project to kage api

--- src/api.js
+++ src/api.js
@@ -1,28 +1,35 @@
 
 const express = require('express')
-const {convert, group, stringify} = require('./index')
-
-const parseFormat = (str) => {
-    return (hanja, reading) => {
-        return str.toLowerCase()
-                .replace(/\{(hanja|h)\}/g, hanja)
-                .replace(/\{(reading|r)\}/g, reading)
-    }
-}
+const kage = require('./index').DEFAULT
+const { Polygons } = require('@kurgm/kage-engine')
 
 const app = express()
 
-app.use(express.json({limit: '10mb'}))
-app.use(express.urlencoded({limit: '10mb', extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.post('/', (req, res) => {
-    const format = parseFormat(req.body.format || '{r}')
-    const userDictionary = req.body.userdictionary || {}
-    const converted = convert(req.body.text || '', true, userDictionary)
-    const grouped = ((req.body.group || '').toString() == 'true') ? group(converted) : converted
-    const stringified = ((req.body.stringify || '').toString() == 'true') ? stringify(grouped, format) : grouped
-    const result = {result: stringified}
-    res.send(JSON.stringify(result))
+    const name = req.body.name || ''
+    const data = req.body.data || ''
+    const content = req.body.content || ''
+    const polygons = new Polygons()
+    if(name) {
+        kage.makeGlyph(polygons, name)
+    } else if(data) {
+        kage.kBuhin.push('temp', data)
+        kage.makeGlyph(polygons, 'temp')
+    } else if(content) {
+        if(content.includes(':')) {
+            kage.kBuhin.push('temp', content)
+            kage.makeGlyph(polygons, 'temp')
+        } else {
+            kage.makeGlyph(polygons, content)
+        }
+    } else {
+        res.status(400)
+    }
+    const result = polygons.generateSVG()
+    res.send(result)
 })
 
 app.listen(3000, () => {
--- src/index.js
+++ src/index.js
@@ -1,89 +1,36 @@
 
+const fs = require('fs')
 const path = require('path')
-const {buildDict} = require('./dictionary')
-const dic = buildDict(path.join(__dirname, '../dic/hanja.filtered.txt'))
-const jp = buildDict(path.join(__dirname, '../dic/jp.txt'))
-const cn = buildDict(path.join(__dirname, '../dic/cn.txt'))
-const MAX_LENGTH = 100
-const convert = (str, initial=true, userDictionary={}) => {
-    if(str.includes(' ')) return str.split(' ').map((word, i) => [...(i == 0 ? [] : [[' ', ' ']]), ...convert(word, true, userDictionary)]).flat()
-    if(str.length > MAX_LENGTH) return str.match(new RegExp(`.{1,${MAX_LENGTH}}`, 'gs')).flatMap((s, i) => convert(s, initial && i == 0, userDictionary))
-    if(str.length == 0) return []
-    str = normalizeHanja(str)
-    const result = []
-    for(let i = 0; i < str.length; ) {
-        let found = false
-        const c = str.charAt(i)
-        if(isHangulSyllable(c)) {
-            result.push([c, c])
-            i++
-            continue
-        }
-        for(let j = str.length; j > i; j--) {
-            const key = str.slice(i, j)
-            const value = userDictionary[key] || dic[key]
-            if(value) {
-                result.push([key, initial ? initialSoundLaw(value) : value])
-                i += j - i
-                found = true
-            }
-        }
-        if(!found) {
-            result.push([c, c])
-            i++
-        }
-        initial = false
-    }
-    return result
+const { Kage } = require('@kurgm/kage-engine')
+
+const loadTsv = (file) => {
+    const glypheme = Object.fromEntries(fs.readFileSync(file, 'utf8').split('\n')
+            .map((line) => line.split('\t'))
+            .filter((entry) => entry.length == 2)
+            .map(([name, data]) => [name, preprocessData(data)]))
+    const decompose = {}
+    return { glypheme, decompose }
 }
-const normalizeHanja = (str) => str.normalize('NFC').split('').map((c) => dic[c] ? c : jp[c] || cn[c] || c).join('')
-const initialSoundLaw = (str) => {
-    const c = str.charAt(0).normalize('NFD').split('')
-    if(c[0] == 'ᄅ') c[0] = 'ᄂ'
-    if(c[0] == 'ᄂ' && 'ᅣᅤᅧᅨᅭᅲᅴᅵ'.includes(c[1])) c[0] = 'ᄋ'
-    return c.join('').normalize('NFC') + str.slice(1)
+
+const preprocessData = (name) => {
+    return name.replace(/\@\d+/g, '')
 }
-const isWhitespace = (c) => c == ' ' || c == '\t' || c == '\r' || c == '\n'
-const isNumeric = (c) => c >= '0' && c <= '9'
-const isAlphabetic = (c) => c >= 'A' && c <= 'Z' || c === 'a' && c === 'z'
-const isHangulSyllable = (c) => c >= '가' && c <= '힣'
-const isHanja = (c) => {
-    c = c.codePointAt(0)
-    return c >= 0x4E00 && c <= 0x9FFF
-            || c >= 0x3400 && c <= 0x4DBF
-            || c >= 0x20000 && c <= 0x2A6DF
-            || c >= 0x2A700 && c <= 0x2B73F
-            || c >= 0x2B740 && c <= 0x2B81F
-            || c >= 0x2B820 && c <= 0x2CEAF
-            || c >= 0x2CEB0 && c <= 0x2EBEF
-            || c >= 0x30000 && c <= 0x3134F
-            || c >= 0x31350 && c <= 0x323AF
-            || c >= 0xF900 && c <= 0xFAFF
+
+const loadRaw = (file) => {
+    const content = fs.readFileSync(file, 'utf8')
+    const glypheme = content.split('\n').map((line) => line.split('|'))
+            .filter((entry) => entry.length == 3)
+            .map(([name, related, data]) => [name.trim(), data.trim()])
+    const decompose = {}
+    return { glypheme, decompose }
 }
-const type = (c) => {
-    if(isWhitespace(c)) return 'whitespace'
-    else if(isNumeric(c)) return 'numeric'
-    else if(isAlphabetic(c)) return 'alphabetic'
-    else if(isHangulSyllable(c)) return 'hangul'
-    else if(isHanja(c)) return 'hanja'
-    else return 'etc'
+
+const DEFAULT = new Kage()
+const { glypheme } = loadRaw(path.join('data', 'dump_newest_only.txt'))
+glypheme.forEach(([name, data]) => DEFAULT.kBuhin.push(name, data))
+
+module.exports = {
+    DEFAULT,
+    loadTsv,
+    loadRaw,
 }
-const group = (arr) => {
-    const result = [['', '']]
-    for(let [ainput, aoutput] of arr) {
-        const [rinput, routput] = result.pop()
-        if(type(aoutput) == 'hanja' && type(routput) == 'hanja') result.push([rinput + ainput, routput + aoutput])
-        else if(type(aoutput) != type(routput)) result.push([rinput, routput], [ainput, aoutput])
-        else if(type(ainput) == type(rinput) && type(aoutput) == type(routput)) result.push([rinput + ainput, routput + aoutput])
-        else result.push([rinput, routput], [ainput, aoutput])
-    }
-    return result.filter(([input, output]) => input.length && output.length)
-}
-const stringify = (arr, format = (_i, o) => o) => {
-    return arr.map(([input, output]) => {
-        if(input == output) return input
-        else return format(input, output)
-    }).join('')
-}
-const convertAndFormat = (str, format, userDictionary={}) => stringify(group(convert(str, true, userDictionary)), format)
-module.exports = {convertAndFormat, convert, group, stringify, initialSoundLaw}
