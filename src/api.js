07ebb118ca5e3ff6df890220a78469f06b838db0
Lee0701 <hgl1002@naver.com>
2023-06-02 23:13:15 +0900

    Make glyph building functions

--- src/api.js
+++ src/api.js
@@ -3,6 +3,21 @@ const express = require('express')
 const { getDefault } = require('./index')
 const { Polygons } = require('@kurgm/kage-engine')
 
+const allAscii = (str) => str.split('').filter((c) =>!(c >= '!' && c <= '~')).length == 0
+
+const makeGlyphWithChar = (polygons, kage, char) => {
+    kage.makeGlyph(polygons, 'u' + char.codePointAt(0).toString(16).padStart(4, '0'))
+}
+
+const makeGlyphWithName = (polygons, kage, name) => {
+    kage.makeGlyph(polygons, name)
+}
+
+const makeGlyphWithData = (polygons, kage, data) => {
+    kage.kBuhin.push('temp', data)
+    kage.makeGlyph(polygons, 'temp')
+}
+
 const main = async () => {
     const kage = await getDefault()
     const app = express()
@@ -16,24 +31,16 @@ const main = async () => {
         const data = req.body.data || ''
         const content = req.body.content || ''
         const polygons = new Polygons()
-        if(char) {
-            kage.makeGlyph(polygons, 'u' + char.codePointAt(0).toString(16).padStart(4, '0'))
-        } else if(name) {
-            kage.makeGlyph(polygons, name)
-        } else if(data) {
-            kage.kBuhin.push('temp', data)
-            kage.makeGlyph(polygons, 'temp')
-        } else if(content) {
-            if(content.includes(':')) {
-                kage.kBuhin.push('temp', content)
-                kage.makeGlyph(polygons, 'temp')
-            } else if(content.split('').filter((c) => !(c >= '!' && c <= '~')).length) {
-                kage.makeGlyph(polygons, 'u' + content.codePointAt(0).toString(16).padStart(4, '0'))
-            } else {
-                kage.makeGlyph(polygons, content)
-            }
+        if(char) makeGlyphWithData(polygons, kage, char)
+        else if(name) makeGlyphWithName(polygons, kage, name)
+        else if(data) makeGlyphWithData(polygons, kage, data)
+        else if(content) {
+            if(content.includes(':')) makeGlyphWithData(polygons, kage, content)
+            else if(allAscii(content)) makeGlyphWithName(polygons, kage, content)
+            else makeGlyphWithChar(polygons, kage, content)
         } else {
-            res.status(400)
+            res.status(400).send()
+            return
         }
         const result = polygons.generateSVG()
         res.send(result)
